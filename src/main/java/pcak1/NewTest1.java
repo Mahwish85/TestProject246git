28375286d833723a9b31434f4392b003bce44d96
Kamran Zameer <kamranzameer@g.harvard.edu>
2021-10-07 13:26:35 -0400

    this is my first commit

--- src/main/java/pcak1/NewTest1.java
+++ src/main/java/pcak1/NewTest1.java
@@ -1,9 +1,23 @@
 package pcak1;
 
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 public class NewTest1 {
   @Test
   public void f() {
-  }
+	  System.setProperty("webdriver.chrome.driver", "C:\\Users\\Kamran\\Downloads\\chromedriver_win32\\chromedriver.exe");
+		//opens chrome browser
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		
+		driver.get("https://ui.freecrm.com/");
+		
+		String atitle = driver.getTitle();
+		String etitle = "Cogmento CRM";
+		}
 }
